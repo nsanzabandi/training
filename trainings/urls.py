b8160b0f014f9581e84c2d959f1f88cc8c57696b
Daniel <danielnsanzabandi@gmail.com>
2025-05-05 16:14:49 +0200

    db added3

--- trainings/urls.py
+++ trainings/urls.py
@@ -4,7 +4,8 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from views import create_superuser
+from .views import create_superuser
+
 
 urlpatterns = [
     # === Dashboard and Authentication ===
