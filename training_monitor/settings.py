2811e9dbe795f3928c048c4a2b2add9c283589c6
Daniel <danielnsanzabandi@gmail.com>
2025-05-07 16:28:13 +0200

    media_server_added_cloudinary1

--- training_monitor/settings.py
+++ training_monitor/settings.py
@@ -108,6 +108,7 @@ else:
         'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
         'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
         'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+        'RESOURCE_TYPE': 'raw',
     }
     
     MEDIA_URL = f"https://res.cloudinary.com/{os.getenv('CLOUDINARY_CLOUD_NAME')}/"
